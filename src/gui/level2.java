ba97ec53fa0cad193d1959dc05659126f49d3eb0
ravisha2000 <singhabahuravisha2580@gmail.com>
2023-12-04 17:37:45 +0530

    time left updated to 30 seconds

--- src/gui/level2.java
+++ src/gui/level2.java
@@ -27,7 +27,7 @@ import java.awt.event.ActionListener;
 public class level2 extends javax.swing.JFrame {
 
     private Timer timer;
-    private int countDownValue = 20;
+    private int countDownValue = 30;
     
      
      
